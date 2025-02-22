432829f8d5ade78d96fad11c75efbcf330315e0b
Zeyad-Balal <zeyad.hassan.balal@gmail.com>
2025-02-22 15:27:13 +0200

    navbar and routing implemented🧭
    in addition to, installing bootstrap and fontawesome packs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MovieSeriesDetailsComponent } from './components/movie-series-details/movie-series-details.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PlansComponent } from './components/plans/plans.component';
+import { WatchListComponent } from './components/watch-list/watch-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'movies',
+    component: MovieSeriesDetailsComponent,
+    title: 'Movies',
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    title: 'user profile',
+  },
+  {
+    path: 'plans',
+    component: PlansComponent,
+    title: 'plans',
+  },
+  {
+    path: 'watch-list',
+    component: WatchListComponent,
+    title: 'watch list',
+  },
+];

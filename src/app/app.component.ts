d1903e4f264a2034cdd852fe43d16f22278f13c6
Zeyad Hassan <zeyad.hassan.balal@gmail.com>
2025-02-26 16:04:25 +0200

    side menu bar init. design🤔

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { SideMenuComponent } from "./components/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, SideMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

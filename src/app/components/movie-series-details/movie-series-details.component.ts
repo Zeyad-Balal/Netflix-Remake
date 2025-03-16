3421c65ef2cfc12a364ee40f15fbbea11904f098
Zeyad Hassan <zeyad.hassan.balal@gmail.com>
2025-03-16 12:20:08 +0200

    feat: enhance movie series details component with detailed view and improved data handling

--- src/app/components/movie-series-details/movie-series-details.component.ts
+++ src/app/components/movie-series-details/movie-series-details.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavComponent } from "../nav/nav.component";
 import { MovieService } from '../../core/services/movie.service';
 import { ActivatedRoute } from '@angular/router';
 import { IMovieDetails } from '../../core/services/imovie-details';
+import e from 'express';
+import { environment } from '../../core/environments/env';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-series-details',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent, CommonModule, DatePipe, DecimalPipe],
   templateUrl: './movie-series-details.component.html',
   styleUrl: './movie-series-details.component.scss'
 })
 export class MovieSeriesDetailsComponent implements OnInit {
 
-  constructor(private readonly _Movie:MovieService , private readonly _ActiveRoute:ActivatedRoute){}
-  MovieDetails:IMovieDetails[] = [];
+  constructor(private readonly _Movie: MovieService, private readonly _ActiveRoute: ActivatedRoute) { }
+  movieDetails: IMovieDetails | null = null;
+  image_path = environment.image_url;
   ngOnInit() {
-    this._ActiveRoute.params.subscribe((params) => {
-      const movieId = params['id'];
-      if (movieId) {
-        this._Movie.getMovieById(movieId).subscribe((res) => {
-          this.MovieDetails = res;
-          console.log(res);
-        });
-      }
-    });
+    const movieId = this._ActiveRoute.snapshot.paramMap.get('id');
+    if (movieId) {
+      this._Movie.getMovieById(movieId).subscribe((res) => {
+        this.movieDetails = res;
+        console.log(res);
+      });
+    }
+
   }
 
 }

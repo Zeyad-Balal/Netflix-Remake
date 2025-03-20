b66fda54fa2f49c93a145dd0025bd5f2f90758b9
Zeyad Hassan <zeyad.hassan.balal@gmail.com>
2025-03-20 11:19:48 +0200

    feat: enhance Plans component with selection and dimming effects for plan cards

--- src/app/components/plans/plans.component.ts
+++ src/app/components/plans/plans.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-plans',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './plans.component.html',
   styleUrl: './plans.component.scss'
 })
 export class PlansComponent {
+  selectedPlan: 'basic' | 'standard' | 'premium' = 'premium';
 
+  selectPlan(plan: 'basic' | 'standard' | 'premium') {
+    this.selectedPlan = plan;
+  }
 }
